89ce0b8706f4325c0bac1631939c0682e8545038
Tony Hignett <tony.hignett@scivisum.co.uk>
2017-04-25 13:44:58 +0100

    [#144267611] Include exchanges in HARs even when they're not complete.
    - Build up the entry using whatever events we've received.
    - Don't assume that an exchange with an ended event had a started one, because we believe this happens for some exchanges.
    - Synchronise some things with browsermob-proxy:
       - use the same status codes for unfinished (-999) and error (-998);
       - use the same error codes (e.g. `CONNECTION_TIMED_OUT`);
       - include a `state` field in each entry.

--- src/third_party/har.js
+++ src/third_party/har.js
@@ -44,109 +44,106 @@ exports.createHar = function (page, resources) {
 
         if (error) {
             // according to http://qt-project.org/doc/qt-4.8/qnetworkreply.html
+            // Synchronised with browsermob-proxy.
             switch (error.errorCode) {
                 case 1:
-                    error.errorString = '(refused)';
+                    error.errorCode = "CONNECTION_REFUSED";
                     break;
                 case 2:
-                    error.errorString = '(closed)';
+                    error.errorCode = "CONNECTION_CLOSED";
                     break;
                 case 3:
-                    error.errorString = '(host not found)';
+                    error.errorCode = "UNKNOWN_HOST";
                     break;
                 case 4:
-                    error.errorString = '(timeout)';
+                    error.errorCode = "CONNECTION_TIMED_OUT";
                     break;
                 case 5:
-                    error.errorString = '(canceled)';
+                    error.errorCode = "OPERATION_CANCELLED";
                     break;
                 case 6:
-                    error.errorString = '(ssl failure)';
+                    error.errorCode = "SSL";
                     break;
-                case 7:
-                    error.errorString = '(net failure)';
+                default:
+                    error.errorCode = "CONNECTION_GENERIC";
                     break;
             }
         }
 
-        if (startReply && endReply) {
-            entries.push({
-                startedDateTime: request.time.toISOString(),
-                time: endReply.time - request.time,
-                request: {
-                    method: request.method,
-                    url: request.url,
-                    httpVersion: "HTTP/1.1",
-                    cookies: [],
-                    headers: request.headers,
-                    queryString: [],
-                    headersSize: -1,
-                    bodySize: -1
+        var entry = {
+            startedDateTime: request.time.toISOString(),
+            time: null,
+            state: "wait",
+            request: {
+                method: request.method,
+                url: request.url,
+                httpVersion: "HTTP/1.1",
+                cookies: [],
+                headers: request.headers,
+                queryString: [],
+                headersSize: -1,
+                bodySize: -1
+            },
+            response: {
+                status: -999,
+                statusText: null,
+                httpVersion: "HTTP/1.1",
+                cookies: [],
+                headers: {},
+                redirectURL: "",
+                headersSize: -1,
+                bodySize: null,
+                content: {
+                    size: null,
+                    mimeType: null
                 },
-                response: {
-                    status: error ? null : endReply.status,
-                    statusText: error ? error.errorString : endReply.statusText,
-                    httpVersion: "HTTP/1.1",
-                    cookies: [],
-                    headers: endReply.headers,
-                    redirectURL: "",
-                    headersSize: -1,
-                    bodySize: startReply.bodySize,
-                    content: {
-                        size: startReply.bodySize,
-                        mimeType: endReply.contentType
-                    }
-                },
-                cache: {},
-                timings: {
-                    blocked: 0,
-                    dns: -1,
-                    connect: -1,
-                    send: 0,
-                    wait: startReply.time - request.time,
-                    receive: endReply.time - startReply.time,
-                    ssl: -1
-                },
-                pageref: page.url
-            });
-        } else if (error) {
-            entries.push({
-                startedDateTime: request.time.toISOString(),
-                time: 0,
-                request: {
-                    method: request.method,
-                    url: request.url,
-                    httpVersion: "HTTP/1.1",
-                    cookies: [],
-                    headers: request.headers,
-                    queryString: [],
-                    headersSize: -1,
-                    bodySize: -1
-                },
-                response: {
-                    status: null,
-                    statusText: error.errorString,
-                    httpVersion: "HTTP/1.1",
-                    cookies: [],
-                    headers: [],
-                    redirectURL: "",
-                    headersSize: -1,
-                    bodySize: 0,
-                    content: {}
-                },
-                cache: {},
-                timings: {
-                    blocked: 0,
-                    dns: -1,
-                    connect: -1,
-                    send: 0,
-                    wait: 0,
-                    receive: 0,
-                    ssl: -1
-                },
-                pageref: page.url
-            });
+                error: {}
+            },
+            cache: {},
+            timings: {
+                blocked: 0,
+                dns: -1,
+                connect: -1,
+                send: 0,
+                wait: -1,
+                receive: -1,
+                ssl: -1
+            },
+            pageref: page.url
+        };
+
+        function fillInHeaders (reply) {
+            entry.response.status = reply.status;
+            entry.response.statusText = reply.statusText;
+            entry.response.headers = reply.headers;
+            entry.response.content.mimeType = reply.contentType;
+            entry.response.bodySize = reply.bodySize;
+            entry.response.content.size = reply.bodySize;
         }
+
+        if (startReply) {
+            entry.timings.wait = startReply.time - request.time;
+            entry.state = "receive";
+            fillInHeaders(startReply);
+        }
+
+        if (error) {
+            entry.response.status = -998;
+            entry.response.statusText = error.errorString;
+            entry.response.error.code = error.errorCode;
+            entry.response.error.message = error.errorString;
+        } else if (endReply) {
+            entry.time = endReply.time - request.time;
+            if (startReply) {
+                // Certain requests (redirects, synchronous AJAX requests) will not receive
+                // onResourceReceived with stage "start".
+                entry.timings.receive = endReply.time - startReply.time;
+            } else {
+                fillInHeaders(endReply);
+            }
+        }
+
+        entries.push(entry);
     });
 
     return {
